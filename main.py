d1d008f9c5e7a50a0b9fc7d4e0c85c2f4e688e55
vaivinc <168576062+vaivinc@users.noreply.github.com>
2024-10-03 18:02:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from datetime import datetime
 app = FastAPI()
 
 @app.get("/path/{user_id}/")
-async def index(user_id: int = Path(description="user_id"),
-                timestamp: Optional[str] = Query(None),
-                x_client_version: int = Header()):
+async def index(user_id: int = Path(description="Enter your id, Example: 1"),
+                timestamp: Optional[str] = Query(None, description="Enter your time, Example: 12:49"),
+                x_client_version: int = Header(description="Enter your client_version, Example: 3.1.1")):
     if not timestamp:
         timestamp = datetime.now().isoformat()
+
     return {
         "user_id": user_id,
         "timestamp": timestamp,
